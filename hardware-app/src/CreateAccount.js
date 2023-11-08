36b5c5e7502b7fb453cf67f6b354b9c752ea6297
reeseroo <reesewelty@utexas.edu>
2023-11-07 21:00:53 -0600

    Working on the Create User
    I have the basic design. I am testing if it works with MongoDB and if it blocks repeat users

--- hardware-app/src/CreateAccount.js
+++ hardware-app/src/CreateAccount.js
@@ -3,8 +3,86 @@ import OutlinedInput from "@mui/material/OutlinedInput";
 import Button from "@mui/material/Button";
 import React, {useState} from "react";
 
-function CreateAccount (){
+function Create (){
+const [user, setUser] = useState("");
+    const[pass, setPass] = useState("");
+    const[answer, setAnswer] = useState("");
+    const navigate = useNavigate();
 
+    const handleRegister = async(event) => {
+        event.preventDefault();
+        try {
+            const response = await fetch("http://localhost:5000/addUser", {
+                method: "POST",
+                body: JSON.stringify({
+                    username: user,
+                    password: pass
+                }),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+
+            if (response.status === 200) {
+                navigate('/Projects', {state:{"user" :  user}});
+                alert('User successfully created.');
+            } else {
+                setUser("");
+                setPass("");
+                alert('Unique username Required. Please try again.');
+            }
+            } catch (error) {
+                setUser("");
+                setPass("");
+                alert('An error occurred, please try again later.');
+        }
+    };
+
+
+    return (<div>
+            <div className = 'addMargin'>
+               <OutlinedInput
+                        size="small"
+                        id="component-outlined"
+                        placeholder = "Username"
+                        value = {user}
+                        onChange={(event) => {
+                            setUser(event.target.value);
+                        }}
+                />
+            </div>
+            <div className = 'addMargin'>
+                <OutlinedInput
+                        size="small"
+                        id="component-outlined"
+                        placeholder = "Password"
+                        type ="password"
+                        value = {pass}
+                        onChange={(event) => {
+                            setPass(event.target.value)
+                        }}
+                />
+            </div>
+            <div className = 'addMargin'>
+                <Button variant="outlined" onClick = {handleRegister}>
+                    Register
+                </Button>
+            </div>
+        </div>
+    );
 }
+
+function CreateAccount (){
+    return(
+        <div className = "centered">
+            <h2>Create Your Account Here.</h2>
+            <Create/>
+            <div className = "centerText">
+                <Link to= "/">Return to login</Link>
+            </div>
+        </div>
+    )
+}
+
 
 export default CreateAccount;
